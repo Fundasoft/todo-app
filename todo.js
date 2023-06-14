4bbb8ca9f187dbe3ecefe816bb9722abb88c1a1b
rotjo <diego.rotharmel@gmail.com>
2023-06-14 20:57:43 -0300

    Added create node task function

--- todo.js
+++ todo.js
@@ -1,2 +1,16 @@
-var elements = window.document.getElementsByTagName('li');
-elements[0].style.color = 'red';
+function createTask(text) {
+    var li = document.createElement('li');
+    var input = document.createElement('input');
+    var div = document.createElement('div');
+    var texto = document.createTextNode(text);
+
+    li.className = 'list__item';
+    input.type = 'checkbox';
+    div.className = 'list__text';
+
+    div.appendChild(texto);
+    li.appendChild(input);
+    li.appendChild(div);
+
+    return li;
+}
